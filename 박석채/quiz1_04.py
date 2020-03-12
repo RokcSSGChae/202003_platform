60a20641e3a4efe91813ae5fbf547763fb8287b4
park seok chae <tjrco56@naver.com>
2020-03-12 19:32:10 +0900

    박석채

---
+++ 박석채/quiz1_04.py
@@ -0,0 +1,8 @@
+"""4. 삼각형의 가로와 높이를 받아서 넓이를 출력하는 함수를 작성하시오."""
+
+garo = float(input("가로 길이를 입력 : "))
+sero = float(input("세로 길이를 입력 : "))
+
+area = garo*sero*0.5
+
+print('삼각형의 넓이는 {}입니다.'.format(area))
